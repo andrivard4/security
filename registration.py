7c32cdc75887387cff58293b81ebbcda5c15ccf9
Andrew Rivard <andrivard4@icloud.com>
2020-10-23 18:01:08 -0400

    offical start to ms1

--- registration.py
+++ registration.py
@@ -1,4 +1,31 @@
+#remember to install https://pypi.org/project/pycryptodome/
 from Crypto.Hash import SHA256
-hash = SHA256.new()
-hash.update('message')
-print(hash.hexdigest())
+
+username = "usr"
+email = "email"
+password = "pswd"
+confirm = "conf"
+
+#Pooja
+def getInput() :
+    #Get user input from command line
+    #Save it in the above variables
+    print("getInput")
+
+#Cassie
+def validateInput() :
+    #validates the input
+    # ex email is an email, password match...
+    print("validateInput")
+
+# Andrew
+def encryptData() :
+    #Andrew encrypts the user data
+    hash = SHA256.new()
+    hash.update('message')
+    print(hash.hexdigest())
+
+#Abhi
+def loadFile() :
+    #Creates a file and saves the user data
+    print("loadFile")
