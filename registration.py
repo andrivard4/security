81f9abae25e1ec756c04c6991e0ff537b257fa92
Cassandra Harkins <cassandraharkins@Cassandras-MacBook-Air.local>
2020-10-24 20:18:41 -0400

    Validate Credentials Done

--- registration.py
+++ registration.py
@@ -11,6 +11,7 @@ salt = 0
 password = "Password"
 confirm = "conf"
 
+
 #Pooja
 def getInput() :
     #Get user input from command line
@@ -29,7 +30,49 @@ def validateInput() :
     #once passwords are confirmed set confirm = "" because we no longer want it in memory ~Andrew
     #validates the input
     # ex email is an email, password match...
-    print("validateInput")
+    global username;    global email;
+    global password;    global confirm;
+    has_digit = 0;    has_symbol = 0;
+    has_upper = 0;    has_lower = 0;
+    error = 1;
+    
+    while error == 1 or errormess != "":
+        error = 0; errormess = "";
+        print("Validating Input\n")
+    
+        if len(email.split("@")) != 2 :
+            errormess += "Email is invalid\n"
+            error = 1
+        if password != confirm :
+            errormess += "Passwords do not match\n"
+            error = 1
+        confirm = ""
+
+        if len(password) < 8 or len(password) > 100 :
+            errormess += "Password needs to be 8-100 characters in length\n"
+            error = 1
+        else:
+            for character in password:
+                if character.isdigit() :
+                    has_digit = 1
+                if character.islower() :
+                    has_lower = 1
+                if character.isupper() :
+                    has_upper = 1
+                if not character.isalnum() and not character.isspace:
+                    has_symbol = 1;
+                if character.isspace() :
+                    errormess += "Password cannot contain white space"
+                    error = 1;
+        if (has_digit + has_lower + has_upper + has_symbol) < 3 :
+            errormess += "Password needs 3 of the following:\n number, uppercase letter, lowercase letter, symbol\n"
+            error = 1
+        if(errormess == "" and error == 0) :
+            print("Credentials verified, creating contact\n")
+        else :
+            print(errormess)
+            getInput()
+
 
 # Andrew
 def encryptData() :
