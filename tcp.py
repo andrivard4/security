63265542e86bb0e1205778caa26849eb0529ac99
Cassandra Harkins <cassandraharkins@Cassandras-Air.fios-router.home>
2020-11-26 16:56:04 -0500

    creation of ms4

---
+++ tcp.py
@@ -0,0 +1,26 @@
+
+from Crypto.PublicKey import RSA
+from Crypto.Random import get_random_bytes
+from Crypto.Cipher import AES, PKCS1_OAEP
+import sys
+import os
+import json
+
+
+ 
+# Cassie, Pooja
+# Given a list of ports, send TCP to each with hashed email
+# Wait for reply
+# Verify reply hash exists in contact email list
+# If yes, add contact and port to list of online users
+def checkForOnlineContacts:
+
+
+
+# Cassie, Pooja
+# Check hashed email with emails you have (hashed)
+# If a matche, note the port that sent request with the contact that matches
+# Send your hashed email back as reply
+def checkIfContact:
+
+
